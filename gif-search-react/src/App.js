97d014605a0b5a90584806d47487af99636b9bd4
Meghan Martin <meghanxmartin@gmail.com>
2020-04-26 17:54:30 +0200

    adds check for search

--- gif-search-react/src/App.js
+++ gif-search-react/src/App.js
@@ -3,13 +3,20 @@ import Header from './Header';
 import './css/main.css';
 
 class App extends Component {
+  handleChange = event => {
+    const {value} = event.target
+    if (value.length > 2) {
+      console.log("search term")
+    }
+  }
+
   render() {
     return (
       <div className="page">
         <Header />
         <div className='search grid'>
           {}
-          <input className='input grid-item' placeholder='Type something'/>
+          <input className='input grid-item' placeholder='Type something' onChange={this.handleChange}/>
         </div>
       </div>
     );
