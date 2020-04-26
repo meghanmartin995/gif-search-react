58f7b260eb03102e2a0a161b938e7792afbf044d
Meghan Martin <meghanxmartin@gmail.com>
2020-04-26 18:10:00 +0200

    adds search term as state

--- gif-search-react/src/App.js
+++ gif-search-react/src/App.js
@@ -4,9 +4,20 @@ import './css/main.css';
 
 class App extends Component {
 
+  constructor(props) {
+    super(props)
+    this.state = {
+      searchTerm: ''
+    }
+  }
+
 
   handleChange = event => {
     const {value} = event.target
+    this.setState((prevState, props) => ({
+      ...prevState,
+      searchTerm: value
+    }))
     if (value.length > 2) {
       console.log("search term")
     }
@@ -15,11 +26,12 @@ class App extends Component {
   handleKeyPress = event => {
     const {value} = event.target
     if (value.length > 2 && event.key === 'Enter'){
-      alert(`search for ${value}`)
+      alert(`search for ${value}`);
     }
   }
 
   render() {
+    const { searchTerm } = this.state
     return (
       <div className="page">
         <Header />
@@ -29,7 +41,8 @@ class App extends Component {
             className='input grid-item'
             placeholder='Type something'
             onChange={this.handleChange}
-            onKeyPress={this.handleKeyPress}/>
+            onKeyPress={this.handleKeyPress}
+            value={searchTerm}/>
         </div>
       </div>
     );
